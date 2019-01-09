cd934e8b1127c6bf6a72681121819ca55673d50f
tkorignal <tauqueer.khan@gmail.com>
2019-01-09 16:37:41 +0000

    Sucessfully split strings into individual letters

---
+++ countingLetters.js
@@ -0,0 +1,7 @@
+function countLetters (string) {
+  let finalOutput = {};
+  stringArray = string.split(' ').join('').toLowerCase().split('');
+  console.log(stringArray);
+}
+
+countLetters('THis is a test');
